1df4b7f2a35f9d27b25dd544332076d5a0d504a3
ZhengJieGan <ganzj45@gmail.com>
2023-06-17 14:18:00 +0800

    calendar

--- src/components/calendar/calendar.tsx
+++ src/components/calendar/calendar.tsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
-import { Calendar as AntCalendar, Card, Breadcrumb, Typography } from "antd";
+import {
+  Calendar as AntCalendar,
+  Card,
+  Breadcrumb,
+  Typography,
+  Alert,
+} from "antd";
 import type { Dayjs } from "dayjs";
 import type { CalendarMode } from "antd/es/calendar/generateCalendar";
 
 const Calendar = () => {
   const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
+  const [currentMonth, setCurrentMonth] = useState<Dayjs | null>(null);
 
   const onPanelChange = (value: Dayjs, mode: CalendarMode) => {
-    console.log(value.format("YYYY-MM-DD"), mode);
+    if (mode === "month") {
+      setSelectedDate(null);
+      setCurrentMonth(value);
+    } else {
+      setSelectedDate(value);
+    }
   };
 
   const selectDateHandler = (e: Dayjs) => {
@@ -31,6 +43,29 @@ const Calendar = () => {
     // Render default content when no date is selected
   };
 
+  const dateCellRender = (date: Dayjs) => {
+    const warningDates = [
+      { day: 8, month: 5, year: 2023 }, // June 8, 2023
+      { day: 9, month: 6, year: 2023 }, // June 8, 2023
+      { day: 12, month: 7, year: 2023 }, // August 12, 2023
+      { day: 15, month: 9, year: 2023 }, // October 15, 2023
+    ];
+
+    const isWarningDate = warningDates.some(
+      ({ day, month, year }) =>
+        date.date() === day && date.month() === month && date.year() === year
+    );
+
+    if (isWarningDate) {
+      return (
+        <div className="tw-relative">
+          <Alert message="Warning" type="warning" showIcon />
+        </div>
+      );
+    }
+    // return "Good to go"
+  };
+
   const breadcrumbItems = selectedDate
     ? [
         <Breadcrumb.Item key="calendar" onClick={removeDateHandler}>
@@ -45,12 +80,13 @@ const Calendar = () => {
   return (
     <Card className="tw-m-4">
       <Breadcrumb>{breadcrumbItems}</Breadcrumb>
-      {!selectedDate && (
-        <AntCalendar
-          onPanelChange={onPanelChange}
-          onSelect={(e) => selectDateHandler(e)}
-        />
-      )}
+
+      <AntCalendar
+        onPanelChange={onPanelChange}
+        onSelect={(e) => selectDateHandler(e)}
+        dateCellRender={dateCellRender}
+      />
+
       {renderContent()}
     </Card>
   );
