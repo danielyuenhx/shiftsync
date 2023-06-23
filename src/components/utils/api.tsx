7237476a2f7ca0f9f58b29271110a031bbb47ec5
Daniel Yuen <yuendaniel33@gmail.com>
2023-06-24 02:48:27 +0800

    updated api date

--- src/components/utils/api.tsx
+++ src/components/utils/api.tsx
@@ -12,8 +12,8 @@ export const sendCalendarShifts = () => {
         location:
           'ShiftSync Starbucks',
         description: 'x1 Barista, x1 Cashier, x2 Waiter',
-        startTime: '2023-06-22T08:00:00+08:00',
-        endTime: '2023-06-22T18:00:00+08:00',
+        startTime: '2023-06-25T08:00:00+08:00',
+        endTime: '2023-06-25T18:00:00+08:00',
         emails: ['shiftsyncdemoemployee@gmail.com', 'daniel@gmail.com', 'zhengjie@gmail.com', 'jessica@gmail.com'],
       },
     ],
