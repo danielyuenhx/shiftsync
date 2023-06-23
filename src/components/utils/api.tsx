4d6e1cf5d64be91bfcd70709637337200555ffc0
Daniel Yuen <yuendaniel33@gmail.com>
2023-06-24 02:26:24 +0800

    remove extra emails

--- src/components/utils/api.tsx
+++ src/components/utils/api.tsx
@@ -14,25 +14,7 @@ export const sendCalendarShifts = () => {
         description: 'x1 Barista, x1 Cashier, x2 Waiter',
         startTime: '2023-06-22T08:00:00+08:00',
         endTime: '2023-06-22T18:00:00+08:00',
-        emails: ['shiftsyncdemoemployee@gmail.com', 'daniel@gmail.com, zhengjie@gmail.com, jessica@gmail.com'],
-      },
-      {
-        title: 'Afternoon Shift',
-        location:
-          'ShiftSync Starbucks',
-        description: 'x1 Cashier, x1 Waiter',
-        startTime: '2023-06-22T12:00:00+08:00',
-        endTime: '2023-06-22T22:00:00+08:00',
-        emails: ['stacy@gmail.com', 'kaiwen@gmail.com'],
-      },
-      {
-        title: 'Full-day',
-        location:
-          'ShiftSync Starbucks',
-        description: 'x1 Barista, x1 Waiter',
-        startTime: '2023-06-22T08:00:00+08:00',
-        endTime: '2023-06-22T22:00:00+08:00',
-        emails: ['nicholas@gmail.com', 'davelee@gmail.com'],
+        emails: ['shiftsyncdemoemployee@gmail.com', 'daniel@gmail.com', 'zhengjie@gmail.com', 'jessica@gmail.com'],
       },
     ],
   });
