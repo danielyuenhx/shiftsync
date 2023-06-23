09de710c2b7a3eeab346d0df5d96148fd59423c8
Daniel Yuen <yuendaniel33@gmail.com>
2023-06-24 01:42:26 +0800

    more stuff

--- src/components/calendar/calendarShiftBlock/calendarShiftBlock.tsx
+++ src/components/calendar/calendarShiftBlock/calendarShiftBlock.tsx
@@ -1,6 +1,6 @@
-import { useAppDispatch, useAppSelector } from "../../../redux/hooks/hooks";
-import { Typography } from "antd";
-import { updateShift } from "../../../redux/slices/demoSlice";
+import { useAppDispatch, useAppSelector } from '../../../redux/hooks/hooks';
+import { Typography } from 'antd';
+import { updateShift } from '../../../redux/slices/demoSlice';
 
 type props = {
   blockIndex: number;
@@ -35,34 +35,34 @@ const CalendarShiftBlock = ({
     <div
       onClick={shiftSelect}
       style={{
-        position: "absolute",
+        position: 'absolute',
         height: `${heightPerHour * (endTime - startTime)}px`,
         width: blockWidth,
-        top: "36px",
+        top: '36px',
         marginTop: `${heightPerHour * startTime}px`,
-        left: "104px",
+        left: '104px',
         marginLeft: `${blockIndex * skipWidth}px`,
-        backgroundColor: shift === selectedShift ? `${colour}` : "lightgray",
+        backgroundColor: shift === selectedShift ? `${colour}` : 'lightgray',
         zIndex: 2,
-        borderRadius: "6px",
-        cursor: "pointer",
+        borderRadius: '6px',
+        cursor: 'pointer',
         // border: shift === selectedShift ? "2px solid gray" : "",
-        transition: "0.25s"
+        transition: '0.25s',
       }}
     >
       <p
         style={{
-          position: "absolute",
+          position: 'absolute',
           bottom: 0,
-          transformOrigin: "bottom left",
-          rotate: "-90deg",
+          transformOrigin: 'bottom left',
+          rotate: '-90deg',
           width: `${heightPerHour * (endTime - startTime) - 10}px`,
           transform: `translate(5px, ${blockWidth - 5}px)`,
           fontWeight: 500,
         }}
       >
-        <div className="tw-flex tw-justify-center tw-align-middle ">
-          <Typography className="tw-font-bold">{shiftName}</Typography>
+        <div className='tw-flex tw-justify-center tw-align-middle'>
+          <Typography className='tw-font-bold'>{shiftName}</Typography>
         </div>
       </p>
     </div>
